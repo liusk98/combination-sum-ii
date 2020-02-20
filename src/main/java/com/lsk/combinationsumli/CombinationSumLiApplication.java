c13f09a4d1bab76ee7e88ce726f110a26dddf050
liusk98 <liushaokang98@gmail.com>
2020-02-20 17:51:38 +0800

    init

---
+++ src/main/java/com/lsk/combinationsumli/CombinationSumLiApplication.java
@@ -0,0 +1,13 @@
+package com.lsk.combinationsumli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CombinationSumLiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CombinationSumLiApplication.class, args);
+    }
+
+}
